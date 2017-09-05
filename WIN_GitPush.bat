494e0830d2b779de958b1acc8e804630591e8c38
Atom735 <Atom735.2013@gmail.com>
2017-09-05 19:20:59 +0300

    test push by auto *.bat

--- WIN_GitPush.bat
+++ WIN_GitPush.bat
@@ -2,4 +2,6 @@ IF "%LOCAL_SETTINGS%" == "1" GOTO settings_accepted
 CALL WIN_Settings.bat
 :settings_accepted
 CALL "%TOOL_GIT%" add --all
+CALL "%TOOL_GIT%" commit -a -m %1
+CALL "%TOOL_GIT%" push
 PAUSE
